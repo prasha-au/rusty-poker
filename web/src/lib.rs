3f37c18cd211f9561a02397c8b7a19459fa9f2ca
Prashanth Santhirakumaran <contact@prasha.com.au>
2023-08-30 20:42:11 +0800

    Savepoint.

--- web/src/lib.rs
+++ web/src/lib.rs
@@ -1,8 +1,8 @@
 mod utils;
 
 use wasm_bindgen::prelude::*;
-use rusty_poker_core::{evaluator, deck::Deck};
-
+use rusty_poker_core::{evaluator, deck::Deck, game::{Game, GameState, BettingAction}, card::Card, player::Player};
+use serde::{Serialize, Deserialize};
 
 #[wasm_bindgen]
 extern "C" {
@@ -35,5 +35,89 @@ pub fn chance_to_win(table: u64, hand: u64) -> f32 {
 #[wasm_bindgen]
 pub fn chance_to_win_preflop(hand: u64, num_players: u8) -> f32 {
   evaluator::chance_to_win_preflop(&Deck::from_value(hand), num_players)
+}
+
+
+
+pub struct GameInstance {
+  pub game: Game,
+  // pub opponents: [*mut dyn Player; 8],
+}
+
+
+#[wasm_bindgen]
+pub fn create_game() -> *mut GameInstance {
+  Box::into_raw(Box::new(GameInstance {
+    game: Game::create(4, 1000),
+  }))
+}
+
+
+
+
+
+
+
+#[wasm_bindgen]
+pub fn iterate_game(instance: *mut GameInstance) {
+  unsafe { (*instance).game.next() };
+}
+
+
+
+#[derive(Serialize)]
+pub struct PlayerState {
+  pub is_folded: bool,
+  pub wallet: u32,
+  pub money_on_table: u32,
+}
+
+#[derive(Serialize)]
+pub struct MyGameState {
+  pub total_pot: u32,
+  pub wallet: u32,
+  pub value_to_call: u32,
+  pub players: [Option<PlayerState>; 8],
+  pub is_my_turn: bool,
+  pub dealer_index: u8,
+}
+
+
+#[wasm_bindgen]
+pub fn get_game_state(instance: *mut GameInstance) -> JsValue {
+  let state = unsafe { (*instance).game.get_state(0) };
+  serde_wasm_bindgen::to_value(&MyGameState {
+    total_pot: state.total_pot,
+    wallet: state.wallet,
+    value_to_call: state.value_to_call,
+    players: state.players.map(|p_opt| if let Some(p) = p_opt { Some(PlayerState {
+      is_folded: p.is_folded,
+      wallet: p.wallet,
+      money_on_table: p.money_on_table,
+    }) } else { None }),
+    is_my_turn: state.current_player_index.is_some() && state.current_player_index.unwrap() == 0,
+    dealer_index: state.dealer_index,
+  }).unwrap()
+}
+
+
+#[derive(Deserialize)]
+pub struct Action {
+  pub action: String,
+  pub amount: Option<u32>,
+}
+
+
+#[wasm_bindgen]
+pub fn action_player(instance: *mut GameInstance, action: JsValue) {
+  let action: Action = serde_wasm_bindgen::from_value(action).unwrap();
+  let betting_action = match action.action.as_str() {
+    "fold" => BettingAction::Fold,
+    "call" => BettingAction::Call,
+    "raise" => BettingAction::Raise(action.amount.unwrap()),
+    "all_in" => BettingAction::AllIn(action.amount.unwrap()),
+    _ => BettingAction::Fold,
+  };
+  unsafe { (*instance).game.action_current_player(betting_action) }.unwrap();
 }
 
--- web/www/src/index.ts
+++ web/www/src/index.ts
@@ -1,13 +1,23 @@
 import * as wasm from "rusty_poker_web";
 
 wasm.enable_debug();
+console.log(wasm);
 
 console.time('t1');
-console.log(wasm.chance_to_win_preflop(BigInt(0b11000), 5));
+console.log(wasm.chance_to_win_preflop(BigInt(0b11000000000), 5));
 console.timeEnd('t1');
 
 
 console.time('t2');
 console.log(wasm.chance_to_win(BigInt(0b11010), BigInt(0b110000000)));
 console.timeEnd('t2');
+
+
+const game = wasm.create_game();
+console.log(game);
+
+console.log(wasm.iterate_game(game));
+console.log('getting state');
+console.log(wasm.get_game_state(game));
+
 
